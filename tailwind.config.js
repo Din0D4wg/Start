c375bee77a6a2a82fc897a923454a0ac5e3ca44c
unknown <divinaenero24@gmail.com>
2025-05-06 23:56:41 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    './resources/**/*.{js,jsx,vue,blade.php,html}',
+    './**/*.{js,jsx,vue,blade.php,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
