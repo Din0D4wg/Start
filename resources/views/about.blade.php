f5f4f78163dbbd755f6ba9882a67479460b19179
unknown <divinaenero24@gmail.com>
2025-05-08 13:53:11 +0800

    Used tailwind sample to create a better ui

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,13 @@
 <x-layout>
-    <h1 class=" text-4xl font-bold text-red-600 p-4 rounded">Welcome to the Home Page.</h1>
+    <x-slot:title>
+        About
+    </x-slot:title>
+    
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+
+    <h1 class=" text-4xl font-bold text-red-600">Welcome to the Home Page.</h1>
     
 
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,13 @@
 <x-layout>
-    <h1 class=" text-4xl font-bold text-black p-4 rounded">Welcome to the Contact Page.</h1>
+    <x-slot:title>
+        Contact
+    </x-slot:title>
+    
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+
+    <h1 class=" text-4xl font-bold text-black">Welcome to the Contact Page.</h1>
     <p>Here you will find the contact information available.</p>
     <p><strong>Email:</strong>user@gmail.com</p>
 
