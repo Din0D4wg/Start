8a071632f698643fd455ca0e90bdbbf8e8e60ea7
unknown <divinaenero24@gmail.com>
2025-05-23 19:50:53 +0800

    Added update and delete, also used patch instead of put

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -7,9 +7,13 @@
         Job
     </x-slot:heading>
 
-    <h2 class="font-bold text-3xl text-blue-400">{{$job ['title'] }}</h2>
-    <h4 class = "font-semibold text-gray-400 mb-3">Posted By:{{$employer['name']}}</h4>
+    <h2 class="font-bold text-3xl text-blue-400">{{$job->title }}</h2>
+    <h4 class = "font-semibold text-gray-400 mb-3">Posted By:{{$job->employer->name}}</h4>
     <p class="text-xl">
-        This job pays {{$job['salary']}} per year.
+        This job pays {{$job->salary}} per year.
+    </p>
+
+    <p class="mt-6">
+        <x-button href="/jobs/{{$job->id}}/edit">Edit Job</x-button>
     </p>
 </x-layout>
