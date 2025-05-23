1ea93af7f078b13e60c2e32f4db8225edbaa2189
unknown <divinaenero24@gmail.com>
2025-05-23 12:31:55 +0800

    created a job form to add new jobs in the site

--- routes/web.php
+++ routes/web.php
@@ -9,21 +9,31 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function ()  {
-    $jobs = Job::with('employer')->paginate(10);
+    $jobs = Job::with('employer')->latest()->paginate(10);
+    return view ('jobs.index',[ 'jobs' => $jobs]);
+});
 
-    return view ('jobs',[
-        'jobs' => $jobs
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+Route::post('/jobs', function() {
+    //Validation
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
     ]);
+
+    return redirect ('/jobs');
 });
 
 Route::get('/jobs/{id}', function ($id)  {
     $job = Job::with('employer')->findOrFail($id);
-
-    return view('job', [
+    return view('jobs.show', [
         'job' => $job,
-        'employer' => $job->employer,
-    ]);
-
+        'employer' => $job->employer,]);
 });
 
 Route::get('/contact', function () {
