158d4cfc355f0afed1463ddd4cdc2521a9db2466
unknown <divinaenero24@gmail.com>
2025-05-23 15:39:50 +0800

    Added validation to the form as well as error handling

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -19,8 +19,13 @@
           <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">              
-                <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Backend Developer">
+                <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Backend Developer" required>
             </div>
+
+            @error('title')
+            <p class="text-red-600 text-xs font-semibold mt-1">{{$message}}</p>
+            @enderror
+
           </div>
         </div>
        
@@ -29,11 +34,28 @@
           <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">              
-                <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="$50, 000 per year">
+                <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="$50, 000 per year" required>
             </div>
+
+             @error('salary')
+            <p class="text-red-600 text-xs font-semibold mt-1">{{$message}}</p>
+            @enderror
+            
           </div>
         </div>
       </div>
+      <div class="mt-10">
+        {{-- THIS HANDLES THE ERROR. IT ESSENTIALLY DISPLAYS ALL THE ERROR --}}
+            {{-- @if($errors->any())
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li class="text-red-600 text-sm mt-1">{{$error}}</li>
+                @endforeach
+            </ul>
+                
+            @endif --}}
+      </div>
+   
     </div>
   </div>
 
